db17cd8dc681ae38798cb8449ac3a05e9fbfc671
knifeofdreams <knifeofdreams86@gmail.com>
2015-05-25 15:43:23 +0200

    STEP 4 Lambda constants

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,13 +1,9 @@
-def fizzbuzz_of_n(number):
-    if (number % 3 == 0 and number % 5 == 0):
-        return "FizzBuzz"
-    elif (number % 3 == 0):
-        return "Fizz"
-    elif (number % 5 == 0):
-        return "Buzz"
-    else:
-        return str(number)
-
-
 def fizzbuzz():
-    return map(fizzbuzz_of_n, range(1,101))
+    return map(lambda number: fizzbuzz_lambdas[number % 15](number), range(1,101))
+
+id_ = lambda number: str(number)
+fizz_ = lambda number: "Fizz"
+buzz_ = lambda number: "Buzz"
+fizzbuzz_ = lambda number: "FizzBuzz"
+
+fizzbuzz_lambdas = [fizzbuzz_, id_, id_, fizz_, id_, buzz_, fizz_, id_, id_, fizz_, buzz_, id_, fizz_, id_, id_]

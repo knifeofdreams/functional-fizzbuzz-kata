3dbcfae04e95805a18465f71ffba9286c33cd6b1
knifeofdreams <knifeofdreams86@gmail.com>
2015-05-25 16:00:06 +0200

    STEP 6 Concat

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -6,9 +6,12 @@ def const(constant):
         return lambda number: x
     return return_const(constant)
 
+def concat(f, g):
+    return lambda number: f(number) + g(number)
+
 id_ = lambda number: str(number)
 fizz_ = const("Fizz")
 buzz_ = const("Buzz")
-fizzbuzz_ = const("FizzBuzz")
+fizzbuzz_ = concat(fizz_, buzz_)
 
 fizzbuzz_lambdas = [fizzbuzz_, id_, id_, fizz_, id_, buzz_, fizz_, id_, id_, fizz_, buzz_, id_, fizz_, id_, id_]

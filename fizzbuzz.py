500272e5cd9f06bf047647095d237b89f61db81f
knifeofdreams <knifeofdreams86@gmail.com>
2015-05-25 15:31:31 +0200

    Extract fizzbuzz_of_n

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,14 +1,16 @@
+def fizzbuzz_of_n(number):
+    if (number % 3 == 0 and number % 5 == 0):
+        return "FizzBuzz"
+    elif (number % 3 == 0):
+        return "Fizz"
+    elif (number % 5 == 0):
+        return "Buzz"
+    else:
+        return str(number)
+
+
 def fizzbuzz(number = 100):
     if number < 1:
         return []
 
-    if (number % 3 == 0 and number % 5 == 0):
-        result = "FizzBuzz"
-    elif (number % 3 == 0):
-        result = "Fizz"
-    elif (number % 5 == 0):
-        result = "Buzz"
-    else:
-        result = str(number)
-
-    return fizzbuzz(number - 1) + [result]
+    return fizzbuzz(number - 1) + [fizzbuzz_of_n(number)]

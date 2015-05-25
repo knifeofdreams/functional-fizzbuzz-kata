1799596f7eb7ea0bf1b0b301f4609e3626de3cd2
knifeofdreams <knifeofdreams86@gmail.com>
2015-05-25 15:48:13 +0200

    STEP 5 Const generation extracted

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,14 @@
 def fizzbuzz():
     return map(lambda number: fizzbuzz_lambdas[number % 15](number), range(1,101))
 
+def const(constant):
+    def return_const(x):
+        return lambda number: x
+    return return_const(constant)
+
 id_ = lambda number: str(number)
-fizz_ = lambda number: "Fizz"
-buzz_ = lambda number: "Buzz"
-fizzbuzz_ = lambda number: "FizzBuzz"
+fizz_ = const("Fizz")
+buzz_ = const("Buzz")
+fizzbuzz_ = const("FizzBuzz")
 
 fizzbuzz_lambdas = [fizzbuzz_, id_, id_, fizz_, id_, buzz_, fizz_, id_, id_, fizz_, buzz_, id_, fizz_, id_, id_]

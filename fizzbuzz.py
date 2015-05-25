28fec385e0234fb7d890d49e597f49c8a3bc4091
knifeofdreams <knifeofdreams86@gmail.com>
2015-05-25 15:25:43 +0200

    STEP 2 recursion

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,12 +1,14 @@
-def fizzbuzz():
-    result = []
-    for i in range(1,101):
-        if (i % 3 == 0 and i % 5 == 0):
-            result.append("FizzBuzz")
-        elif (i % 3 == 0):
-            result.append("Fizz")
-        elif (i % 5 == 0):
-            result.append("Buzz")
-        else:
-            result.append(str(i))
-    return result
+def fizzbuzz(number = 100):
+    if number < 1:
+        return []
+
+    if (number % 3 == 0 and number % 5 == 0):
+        result = "FizzBuzz"
+    elif (number % 3 == 0):
+        result = "Fizz"
+    elif (number % 5 == 0):
+        result = "Buzz"
+    else:
+        result = str(number)
+
+    return fizzbuzz(number - 1) + [result]
